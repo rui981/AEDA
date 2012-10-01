1604fbc7abc68808b4a7b2126247b139e7f3aae9
Rui Batista <ei06013@gmail.com>
2012-10-01 16:42:48 +0100

    servico standart  falta o non standart

---
+++ Oficina/src/Servico.h
@@ -0,0 +1,32 @@
+#ifndef SERVICO_H
+#define SERVICO_H
+
+#include <string>
+#include <vector>
+using namespace std;
+class Funcionario{
+
+};
+
+
+class Servico{
+private:
+	string data_inicio;
+	string data_fim;
+	vector<Funcionario *> responsaveis;
+public:
+	Servico(string data_inicio, string data_fim);
+
+	virtual ~Servico();
+	string get_data_inicio() const;
+	void set_data_inicio(string data);
+	string get_data_fim() const;
+	void set_data_fim(string data);
+	vector<Funcionario *> get_responsaveis() const;
+	virtual void imprime();
+	void adicionar_funcionario( Funcionario * funcionario);
+};
+
+
+
+#endif
---
+++ Oficina/src/Standart.h
@@ -0,0 +1,28 @@
+#ifndef STANDART_H
+#define STANDART_H
+#include "Servico.h"
+
+class Standart:Servico{
+private:
+	string nome;
+	string descricao;
+	double preco;
+	double duracao;
+private:
+	Standart(string nome, string descricao,double preco, double duracao, string data_inicio, string data_fim);
+	string get_nome() const;
+	void set_nome(string nome);
+	string get_descricao() const;
+	void set_descricao(string descricao);
+	double get_preco() const;
+	void set_preco(double preco);
+	double get_duracao() const;
+	void set_duracao(double duracao);
+	void imprime();
+
+};
+
+
+
+
+#endif
